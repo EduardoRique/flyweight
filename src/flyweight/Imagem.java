1651ee12b9fd5dcb865a33d08dcb535ac40b8c2f
Eduardo Rique <luiseduardorique@gmail.com>
2020-10-09 13:56:47 -0300

    Subindo projeto

---
+++ src/flyweight/Imagem.java
@@ -0,0 +1,14 @@
+package flyweight;
+
+public class Imagem {
+	protected String nomeDaImagem;
+
+	public Imagem(String imagem) {
+		nomeDaImagem = imagem;
+	}
+
+	public void desenharImagem() {
+		System.out.println(nomeDaImagem + " desenhada!");
+	}
+}
+
